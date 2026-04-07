c2be2609b5b827a3aa5e2e699f35b9d6319d9da8
akashflying5-hub <akashflying5@gmail.com>
2026-04-07 16:25:01 +0530

    sample added

---
+++ forgitbash.js
@@ -0,0 +1,9 @@
+// arthametic operation
+let a = 10;
+let b =20;
+console.log(a);
+console.log(b);
+console.log(a+b); //addition
+console.log(a-b); //subraction
+console.log(a/b); //div
+console.log(axb); //modulus
