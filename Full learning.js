636e2071f46d7e8c93f6b249cf12a68eeb065d6a
akashflying5-hub <akashflying5@gmail.com>
2026-04-08 14:57:54 +0530

    my reference

--- Full learning.js
+++ Full learning.js
@@ -13,8 +13,6 @@
 // //c=42// can't be update-TypeError
 // console.log(c) // print value
 
-
-
 // 1et x=18;
 // x++;//inc
 // console.log(x);
@@ -41,5 +39,70 @@
 // console.log(a"b); //multiply
 // console.log(a/b); //div
 // console.log(axb); //modulus
-// console.log(a*"b); //expoential
+// console.log(a**b); //expoential
+
+// Comparision operators
+// console.log(a<b);
+// console.log(a<=b);
+// console.log(a>b);
+// console.log(a>=b);
+
+// assignment operator(=)
+// console.log(a+=b);a=a+b
+// console.log(a-=b);a=a-b
+// console.log(a*=b);a=a*b
+// console.log(a/=b);a=a/b
+// console.log(a%=b);
+
+// strict operator
+// == and == value 
+// // 5==5
+// var a="10";
+// var b = 5;
+// console.log(a==b);
+// check the values and data types
+// var c=20;
+// var d="20";
+// console.log(c==d);
+
+// // console.log(c===d);
+// var f="orange"
+// var g="apple"
+// console.log(f==g);
+// console.log(f===g);
+
+// Not in logical operator
+// var a=20;
+// var b=10;
+// console.log(a!=b);
+
+// let a="8";
+// let b=10;
+// console.log(a==b);
+
+// logical operator
+// 1.and(&&) 2.or(||) 3.Not(!)
+// 1*1=1
+// 1*0=0
+// 0*1=0
+// 0*0=0
+
+// let age=17;
+// // // a?b:c
+// let result=(age>=18&&age<=75)?"he is eligible to vote":"he is not eligible to vote";
+// console.log(result);
+
+// or operator ---> one of the condition is true
+
+// let mark=50;
+// let report=(mark>40||mark<=100)?"he is pass in exam":"he is failed in exam"
+// console.lot(report);
+
+// let age=28;
+// let license=false;
+// if(age>=18||license){console.log("u can't drive")}}else{console.log("u can drive");}
+
+// var price =709;
+// var total =(price>-40)||price<=100)?"sold":"not sold";
+// console.log(total);
 
